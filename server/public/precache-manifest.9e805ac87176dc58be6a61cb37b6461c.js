0b5b0017276d1cd0c5255f333e2cce349d1a3f76
Julian Bajo <jbajopinedo@gmail.com>
2020-05-15 13:43:07 +0200

    fix map error

---
+++ server/public/precache-manifest.9e805ac87176dc58be6a61cb37b6461c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff1f0f349d0d9bd6b07fd73775e24d14",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7099922665700e01bd52",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "50ec47b5b5f910a0732e",
+    "url": "/static/css/main.afd32227.chunk.css"
+  },
+  {
+    "revision": "7099922665700e01bd52",
+    "url": "/static/js/2.b97324c2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.b97324c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50ec47b5b5f910a0732e",
+    "url": "/static/js/main.0a7da2f5.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  }
+]);
