e431f9fcd7cb0fcfce856c3616f123df15a0b09e
Julian Bajo <jbajopinedo@gmail.com>
2020-05-15 11:05:42 +0200

    add images home page

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -8,53 +8,26 @@ import './App.css'
 import Navigation from './ui/navbar/Navbar'
 import PetsList from './pages/pets/pets-List/PetsList'
 import HomePage from './pages/homePage/HomePage'
-// import CoasterList from './pages/coaster-list/CoasterList'
-// import CoasterDetails from './pages/coaster-details/CoasterDetails'
-// import Signup from './pages/signup/Signup'
-// import Login from './pages/login/Login'
-// import Profile from './pages/profile/Profile'
 
-// import AuthService from './../service/auth.service'
 
 
 class App extends Component {
 
-  // constructor() {
-  //   super()
-  //   this.state = { loggedInUser: null }
-  //   this.authService = new AuthService()
-  // }
 
-
-  // setTheUser = userObj => this.setState({ loggedInUser: userObj }, () => console.log('El estado de App ha cambiado:', this.state))
-
-  // fetchUser = () => {
-  //   if (this.state.loggedInUser === null) {
-  //     this.authService.isLoggedIn()
-  //       .then(response => this.setTheUser(response.data))
-  //       .catch(() => this.setTheUser(false))
-  //   }
-  // }
 
 
   render() {
 
-    // this.fetchUser()
 
     return (
       <>
-        <Navigation /*setTheUser={this.setTheUser} loggedInUser={this.state.loggedInUser}*/ />
+        <Navigation />
 
         <main>
 
           <Switch>
             <Route path="/" exact render= {() => <HomePage/> } />
             <Route path="/pets" render= {() => <PetsList/>} />
-            {/* <Route path="/coasters" exact render={() => <CoasterList loggedInUser={this.state.loggedInUser} />} />
-            <Route path="/coasters/:coasterId" render={props => <CoasterDetails {...props} />} />
-            <Route path="/signup" render={props => <Signup {...props} setTheUser={this.setTheUser} />} />
-            <Route path="/login" render={props => <Login {...props} setTheUser={this.setTheUser} />} />
-            <Route path="/profile" render={() => this.state.loggedInUser ? <Profile loggedInUser={this.state.loggedInUser} /> : <Redirect to="/" />} /> */}
           </Switch>
 
         </main>
