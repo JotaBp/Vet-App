4efc62c9934aae514b293ea52313042b96173627
Julian Bajo <jbajopinedo@gmail.com>
2020-05-25 19:33:40 +0200

    build

---
+++ server/public/precache-manifest.95f2967c159b3370576cf8e4bb971d09.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59b81b64309352b020486575c17de5b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ade29e45762802c04b2",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "ead317cb26345576efed",
+    "url": "/static/css/main.300782ed.chunk.css"
+  },
+  {
+    "revision": "9ade29e45762802c04b2",
+    "url": "/static/js/2.fb686392.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.fb686392.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ead317cb26345576efed",
+    "url": "/static/js/main.c8c076a6.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  }
+]);
